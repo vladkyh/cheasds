3adfcbc0d7bc59343224889d349d91b197ed8b59
vladkyh <mraends@gmail.com>
2025-06-10 09:00:12 +0700

    contract colors

--- src/comp/SortDropdown.jsx
+++ src/comp/SortDropdown.jsx
@@ -19,14 +19,14 @@ const SortDropdown = () => {
     };
 
     return (
-        <div className="relative inline-block text-left w-48">
+        <div className="relative inline-block text-left w-48 asdasd">
             <button
                 onClick={toggleDropdown}
-                className="flex items-center justify-between w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none "
+                className="flex items-center justify-between w-full pl-2 pr--3 py-0 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none fdsafasddfas "
             >
-                <span>Sort by: {selectedOption}</span>
+                <span> {selectedOption}</span>
                 <svg
-                    className={`w-5 h-5 ml-2 transition-transform ${isOpen ? 'transform rotate-180' : ''}`}
+                    className={`w-15 h-10 ml-2 transition-transform ${isOpen ? 'transform rotate-180' : ''} fasfasf`}
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
